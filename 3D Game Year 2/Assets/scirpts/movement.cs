344d9f36d992072eee509b628565a31f6e700c78
James Bryan <jbryan55555@gmail.com>
2018-11-10 18:09:15 +0000

    Added basic level and added a character and basic movement to player character

--- 3D Game Year 2/Assets/scirpts/movement.cs
+++ 3D Game Year 2/Assets/scirpts/movement.cs
@@ -1,16 +1,52 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-public class movement : MonoBehaviour {
+public class movement : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float moveSpeed;
+
+
+
+    private Vector3 moveInput;
+    private Vector3 moveVelocity;
+
+    public Rigidbody myRigidBody;
+
+
+    public bool useController;
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+
+
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput * moveSpeed;
+
+
+
+
+
+
+
+
+    }
+
+
+
+    void FixedUpdate()
+    {
+        myRigidBody.velocity = moveVelocity;
+    }
 }

1195b60ce47737b9197984c5033593b875626486
CianDervan <44029453+CianDervan@users.noreply.github.com>
2018-11-12 21:07:57 +0000

    triggers are fucked

---
+++ Overcooked-2.0-week-1/3D Game Year 2/Assets/scirpts/movement.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class movement : MonoBehaviour
+{
+
+    public float moveSpeed;
+
+
+
+    private Vector3 moveInput;
+    private Vector3 moveVelocity;
+
+    public Rigidbody myRigidBody;
+
+
+    public bool useController;
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+
+
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput * moveSpeed;
+
+
+
+
+
+
+
+
+    }
+
+
+
+    void FixedUpdate()
+    {
+        myRigidBody.velocity = moveVelocity;
+    }
+
+
+
+
+}
---
+++ Overcooked-2.0-week-1/3D Game Year 2/Assets/scirpts/triggers.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class triggers : MonoBehaviour
+{
+
+    public bool isImageOn;
+    public Image image1;
+    public Image image2;
+    public Image image3;
+    public Image image4;
+    public Image image5;
+    public Image image6;
+
+    void Start()
+    {
+        isImageOn = false;
+        image1.enabled = false;
+        image2.enabled = false;
+        image3.enabled = false;
+        image4.enabled = false;
+        image5.enabled = false;
+        image6.enabled = false;
+
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.gameObject.tag == "image1")
+        {
+            isImageOn = true;
+        }
+
+        if (other.tag == "image2")
+        {
+            isImageOn = true;
+        }
+
+        if (other.tag == "image3")
+        {
+            isImageOn = true;
+        }
+
+        if (other.gameObject.tag  == "image4")
+        {
+            isImageOn = true;
+        }
+
+        if (other.tag == "image5")
+        {
+            isImageOn = true;
+        }
+
+        if (other.tag == "image6")
+        {
+            isImageOn = true;
+        }
+
+    }
+
+}

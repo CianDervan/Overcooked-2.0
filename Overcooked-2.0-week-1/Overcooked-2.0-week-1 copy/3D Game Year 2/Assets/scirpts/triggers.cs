4fcc814f01b9614cd1b00d4aab85399c59442bef
CianDervan <44029453+CianDervan@users.noreply.github.com>
2018-11-14 20:25:19 +0000

    pictues of flasks plus trigger to take images away

--- Overcooked-2.0-week-1/Overcooked-2.0-week-1 copy/3D Game Year 2/Assets/scirpts/triggers.cs
+++ Overcooked-2.0-week-1/Overcooked-2.0-week-1 copy/3D Game Year 2/Assets/scirpts/triggers.cs
@@ -14,6 +14,14 @@ public class triggers : MonoBehaviour
     public Image image4;
     public Image image5;
     public Image image6;
+    public Image off;
+    public Image off1;
+    public Image off2;
+    public Image off3;
+    public Image off4;
+    public Image off5;
+    public Image off6;
+
 
 
 
@@ -28,6 +36,13 @@ public class triggers : MonoBehaviour
         image4.enabled = false;
         image5.enabled = false;
         image6.enabled = false;
+        off.enabled = true;
+        off1.enabled = true;
+        off2.enabled = true;
+        off3.enabled = true;
+        off4.enabled = true;
+        off5.enabled = true;
+        off6.enabled = true;
 
 
         rend = GetComponent<Renderer>();
@@ -38,40 +53,92 @@ public class triggers : MonoBehaviour
 
     public void OnTriggerEnter(Collider other)
     {
-        
+
         if (other.gameObject.tag == "image1")
         {
             image1.enabled = true;
-          
+
             rend.enabled = true;
 
         }
 
         if (other.tag == "image2")
         {
-            isImageOn = true;
+            image2.enabled = true;
+
+            
         }
 
         if (other.tag == "image3")
         {
-            isImageOn = true;
+            image3.enabled = true;
+
+           
         }
 
-        if (other.gameObject.tag  == "image4")
+        if (other.gameObject.tag == "image4")
         {
-            isImageOn = true;
+            image4.enabled = true;
+
+           
         }
 
         if (other.tag == "image5")
         {
-            isImageOn = true;
+            image5.enabled = true;
+
+        
         }
 
         if (other.tag == "image6")
         {
-            isImageOn = true;
+            image6.enabled = true;
+
+            
         }
 
-    }
+        if (other.tag == "off")
+        {
+            off.enabled = false;
+        }
 
+        if (other.tag == "off")
+        {
+            off1.enabled = false;
+        }
+
+        if (other.tag == "off")
+        {
+            off2.enabled = false;
+        }
+
+        if (other.tag == "off")
+        {
+            off3.enabled = false;
+        }
+
+        if (other.tag == "off")
+        {
+            off3.enabled = false;
+        }
+
+        if (other.tag == "off")
+        {
+            off4.enabled = false;
+        }
+
+        if (other.tag == "off")
+        {
+            off5.enabled = false;
+        }
+
+        if (other.tag == "off")
+        {
+            off6.enabled = false;
+        }
+
+
+
+
+    }
 }

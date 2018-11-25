bd0ef0281174b666b824207ecad73a9404642522
CianDervan <44029453+CianDervan@users.noreply.github.com>
2018-11-25 15:10:44 +0000

    new images added & chemical destroyes when hits the ground and then respawns

--- Overcooked-2.0-week-1/Overcooked-2.0-week-1 copy/3D Game Year 2/Assets/scirpts/PickingUp.cs
+++ Overcooked-2.0-week-1/Overcooked-2.0-week-1 copy/3D Game Year 2/Assets/scirpts/PickingUp.cs
@@ -16,35 +16,44 @@ public class PickingUp : MonoBehaviour {
     public GameObject Chemical1;
     public GameObject tempParent;
     public bool isHolding1 = false;
+    public Transform spawnpoint;
+    public GameObject prefab;
+   
+    
 
     public Image chem1;
 
 
 	// Use this for initialization
-	void Start () {
-
+	void Start ()
+    { 
         chem1.enabled = false;
-		
 	}
+
+    
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 
          Vector3.Distance(Chemical1.transform.position, tempParent.transform.position);
-        if (Distance >= 1f){
+        if (Distance >= 1f)
+        {
             isHolding1 = false;
         }
 
 
 
-        if(isHolding1 == true){
+        if(isHolding1 == true)
+        {
 
             Chemical1.GetComponent<Rigidbody>().velocity = Vector3.zero;
             Chemical1.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
             Chemical1.transform.SetParent(tempParent.transform);
             Chemical1.transform.position = tempParent.transform.position;
 
-        } else {
+        } else
+        {
 
             ObjectPos = Chemical1.transform.position;
             Chemical1.transform.SetParent(null);
@@ -78,25 +87,29 @@ public class PickingUp : MonoBehaviour {
 
     }
 
-    private void OnTriggerEnter(Collider other)
+     void OnTriggerEnter(Collider other)
     {
+        
 
-        if (other.gameObject.tag == "pot"){
-
+        if (other.gameObject.tag == "pot")
+        {
             chem1.enabled = true;
             Destroy(gameObject);
+            Instantiate(prefab, spawnpoint.position, spawnpoint.rotation);
+        }
+
+        if (other.gameObject.tag == "Ground")
+        {
+            chem1.enabled = true;
+            Destroy(gameObject);
+             Instantiate(prefab, spawnpoint.position, spawnpoint.rotation);
         }
 
     }
 
     private void OnMouseUp()
     {
-       
-
-
             isHolding1 = false;
-
-
     }
 
 }
